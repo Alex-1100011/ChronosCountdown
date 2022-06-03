b267176c6e5e42a3c57374811568357d3b652874
Alessandro Alberti <Alessandro.Alberti.Catenaja@gmail.com>
2022-06-03 11:56:36 +0200

    Added navigationBarItems to the ContentView

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -10,26 +10,47 @@ import CoreData
 
 struct ContentView: View {
     @EnvironmentObject var dataController: DataController
-    
+    ///This state controls the size of the ``CounterCardView``
+    @State var isAspectSmall = true
     
     var body: some View {
-        ScrollView {
-            ForEach(dataController.counters){ counter in
-                CounterTopView(counter: counter, type: .showWeeks)
-                    .padding()
-                    .background{
-                        RoundedRectangle(cornerRadius: 30)
-                            .foregroundColor(.blue)
-                    }
+        NavigationView {
+            ScrollView {
+                ForEach(dataController.counters){ counter in
+                    
+                    CounterTopView(counter: counter, type: isAspectSmall ? .showOnlyDays : .showWeeks)
+                        .frame(width: isAspectSmall ? 180 : 360, height: 160, alignment: .leading)
+                        .padding()
+                        .background{
+                            RoundedRectangle(cornerRadius: 30)
+                                .foregroundColor(.blue)
+                        }
+                }
             }
-            Button("Add"){
-                let counter = Counter(
-                    name: "Test",
-                    date: Date() + 60 * 60 * 24 * Double(Int.random(in: 1...60)),
-                    color: .blue,
-                    symbolName: "")
-                dataController.add(counter)
-            }
+            .navigationBarTitle("Counters")
+            //MARK: navigationBarItems
+            .navigationBarItems(trailing:
+             HStack {
+                
+                //Change card size button
+                Button{
+                    isAspectSmall.toggle()
+                } label: {
+                    Label("Change card size", systemImage: isAspectSmall ? "rectangle.grid.2x2" : "rectangle.grid.1x2")
+                }
+                
+                //Add new counter button
+                Button{
+                    let counter = Counter(
+                        name: "Test",
+                        date: Date() + 60 * 60 * 24 * Double(Int.random(in: 1...60)),
+                        color: .blue,
+                        symbolName: "")
+                    dataController.add(counter)
+                } label: {
+                    Label("Add new counter", systemImage: "plus")
+                }
+            })
         }
         
     }
