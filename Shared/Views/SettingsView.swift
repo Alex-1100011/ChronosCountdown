43a1060ae9046a27dc8341c4bfaae4ec8201dceb
Alessandro Alberti <Alessandro.Alberti.Catenaja@gmail.com>
2022-10-10 17:09:57 +0200

    The app Icon can now be changed from the SettingsView

--- Shared/Views/SettingsView.swift
+++ Shared/Views/SettingsView.swift
@@ -14,7 +14,27 @@ struct SettingsView: View {
             List{
                 
                 Section("App icon"){
-                    Text("App icon")
+                    Button("Default Icon"){
+                        setIcon(named: nil)
+                    }
+                    Button("Orange Icon"){
+                        setIcon(named: "AppIcon-Orange")
+                    }
+                    Button("Green Icon"){
+                        setIcon(named: "AppIcon-Green")
+                    }
+                    Button("Black Icon"){
+                        setIcon(named: "AppIcon-Black")
+                    }
+                    Button("Beta Icon"){
+                        setIcon(named: "AppIcon-Beta")
+                    }
+                    Button("Pixel Icon"){
+                        setIcon(named: "AppIcon-Pixel")
+                    }
+                    Button("DALLE Icon"){
+                        setIcon(named: "AppIcon-DALLE")
+                    }
                 }
                 
                 Section("About"){
@@ -26,7 +46,17 @@ struct SettingsView: View {
         }
         
     }
+    
+    func setIcon(named iconName: String?){
+        UIApplication.shared.setAlternateIconName(iconName) { (error) in
+            if let error = error {
+                print("Failed request to update the app’s icon: \(error)")
+            }
+        }
+    }
+    
 }
+
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
