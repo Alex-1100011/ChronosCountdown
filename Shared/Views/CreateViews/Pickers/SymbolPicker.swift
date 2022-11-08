be697c6dfadef095de51a62c502bed0a9e3ff9a3
Alessandro Alberti <Alessandro.Alberti.Catenaja@gmail.com>
2022-11-08 10:52:25 +0100

    The SymbolPicker shows the correct category when the selectedSymbol is changed and adjusted the colour of the page controller

--- Shared/Views/CreateViews/Pickers/SymbolPicker.swift
+++ Shared/Views/CreateViews/Pickers/SymbolPicker.swift
@@ -7,33 +7,58 @@
 
 import SwiftUI
 
+//MARK: SymbolPicker
 ///This `View` lets the user select a symbol, passed with the binding variable ``SymbolPicker/selectedSymbol``
 struct SymbolPicker: View {
     ///The accent color of the elements
-    var color: Color = .blue
+    var color: Color
     ///The symbol selected by the user
     @Binding var selectedSymbol: String
     ///When the search button is tapped this variable turns to `true` and the ``SymbolsView`` should be displayed by the `parent view`
     @Binding var showSearch: Bool
+    @State private var tabSelection = symbolsCategory.allCases[0]
     
     var body: some View {
         
-        TabView {
+        TabView(selection: $tabSelection) {
             ForEach(symbolsCategory.allCases, id: \.self) { category in
                 
                 ScrollView {
                     SymbolGrid(color: color, selectedSymbol: $selectedSymbol, showSearch: $showSearch, category: category)
                 }
                 .tabItem({Label(category.name, systemImage: category.symbol)})
+                .tag(category)
                 
             }
         }
         .frame(height: 300)
         .tabViewStyle(.page)
         .indexViewStyle(.page(backgroundDisplayMode: .always))
+        
+        .onChange(of: selectedSymbol){ newSymbol in
+            updateTabSelection(with: newSymbol)
+        }
+    }
+    
+    func updateTabSelection(with symbolName: String){
+        if let symbol = symbols.first(where: {$0.symbolName == symbolName}){
+            tabSelection = symbol.category
+        }
+    }
+
+    init(color: Color = .blue, selectedSymbol: Binding<String>, showSearch: Binding<Bool>) {
+        self.color = color
+        self._selectedSymbol = selectedSymbol
+        self._showSearch = showSearch
+        
+        //Set the page indicator color and selection
+        UIPageControl.appearance().currentPageIndicatorTintColor = UIColor.secondaryLabel
+        UIPageControl.appearance().pageIndicatorTintColor = UIColor.tertiaryLabel
+        updateTabSelection(with: selectedSymbol.wrappedValue)
     }
 }
 
+//MARK: SymbolGrid
 struct SymbolGrid: View {
     var color: Color
     @Binding var selectedSymbol: String
@@ -66,12 +91,15 @@ struct SymbolGrid: View {
             }
         }
         .padding()
+        //Space for the page control
+        .padding(.bottom, 45)
         .frame(maxHeight: .infinity)
         
         
     }
 }
 
+//MARK: Previews
 struct SymbolPicker_Previews: PreviewProvider {
     static var previews: some View {
         SymbolPicker(selectedSymbol: .constant(symbols[0].symbolName), showSearch: .constant(false))
