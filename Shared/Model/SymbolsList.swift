a5939a1e5fc745694aa1ad92f6424958e5b7b163
Alessandro Alberti <Alessandro.Alberti.Catenaja@gmail.com>
2022-11-08 13:01:53 +0100

    Added new symbols and symbolsCategory, created a preview for the symbols list

--- Shared/Model/SymbolsList.swift
+++ Shared/Model/SymbolsList.swift
@@ -1,103 +1,11 @@
 //
-//  Symbols.swift
+//  SymbolsList.swift
 //  Chronos Countdown
 //
-//  Created by Alessandro Alberti on 08/10/22.
+//  Created by Alessandro Alberti on 08/11/22.
 //
+
 import SwiftUI
-
-///All the categories of symbols
-///
-///This type is used for the ``SymbolsListView``
-enum symbolsCategory: String, CaseIterable{
-    case leisure = "theatermasks"
-    case stationery = "doc.text"
-    case school = "backpack"
-    case tools = "hammer"
-    case objects = "text.book.closed.fill"
-    case music = "music.note"
-    case transportation = "car.fill"
-    case health = "stethoscope"
-    case fitness = "figure.run"
-    case people = "person.fill"
-    
-    ///The Symbol associated to the category
-    var symbol: String {
-        self.rawValue
-    }
-    ///The name of the category
-    var name: String {
-        String(describing: self)
-    }
-    ///Keywords associated to the category
-    ///
-    ///Used to enhance the symbol search
-    var keywords: String {
-        switch self {
-        case .health:
-            return "medicine doctor covid illness"
-        case .objects:
-            return ""
-        case .music:
-            return "song singing concert playing listening"
-        case .transportation:
-            return "trip travel road vacation"
-        case .people:
-            return "person figure friends"
-        case .fitness:
-            return "sport game gym workout activity playing"
-        case .leisure:
-            return "freetime fun relax"
-        case .tools:
-            return "construction building diy"
-        case .school:
-            return "university study"
-        case .stationery:
-            return "school job work study"
-        }
-    }
-}
-
-///This Type holds additional information relevant to `SFSymbols`,
-///such as a name to display, a category and keywords to enhance searching.
-struct Symbol: Hashable{
-    ///The name of the `SFSymbol`
-    var symbolName: String
-    ///A name that describes the symbol, displayed in the ``SymbolsListView``
-    var displayName: String
-    ///The category of the symbol
-    var category: symbolsCategory
-    ///Keywords are used to enhance the symbol search
-    ///
-    ///This `String` combines keywords specific to the symbol, the ``Symbol/symbolName``, the category's ``symbolsCategory/name`` and ``symbolsCategory/keywords``.
-    var keywords: String
-    
-    ///A basic init to use when the ``displayName`` should be the same as ``symbolName``
-    init(_ symbolName: String, _ category: symbolsCategory) {
-        self.init(symbolName, symbolName, category)
-    }
-    ///The standard init
-    init(_ symbolName: String, _ displayName: String, _ category: symbolsCategory) {
-        self.init(symbolName, displayName, category, kw: "")
-    }
-    
-    /// An init to use when there are additional keywords specific to the symbol to add
-    /// - Parameters:
-    ///   - keywords: Additional keywords specific to the symbol
-    init(_ symbolName: String, _ category: symbolsCategory, kw keywords: String) {
-        self.init(symbolName, symbolName, category, kw: keywords)
-    }
-    
-    /// An init to use when there are additional keywords specific to the symbol to add
-    /// - Parameters:
-    ///   - keywords: Additional keywords specific to the symbol
-    init(_ symbolName: String, _ displayName: String, _ category: symbolsCategory, kw keywords: String) {
-        self.symbolName = symbolName
-        self.displayName = displayName
-        self.category = category
-        self.keywords = keywords + symbolName + category.name + category.keywords
-    }
-}
 
 ///A list of symbols, ordered alphabetically relative to the ``Symbol/displayName`` and the ``Symbol/category``
 var symbols: [Symbol] = [
@@ -125,7 +33,53 @@ var symbols: [Symbol] = [
     Symbol("trash", .objects, kw: "delete"),
     Symbol("umbrella", .objects, kw: "weather rain"),
     Symbol("wand.and.rays", "wand", .objects, kw: "magic"),
-
+    
+    
+    //MARK: nature
+    Symbol("globe.americas", .nature),
+    Symbol("globe.europe.africa", .nature),
+    Symbol("globe.asia.australia", .nature),
+    Symbol("globe.central.south.asia", .nature),
+    Symbol("moon", .nature),
+    Symbol("sparkles", .nature),
+    Symbol("moon.stars", .nature),
+    Symbol("cloud", .nature),
+    Symbol("cloud.rain", .nature),
+    Symbol("cloud.snow", .nature),
+    Symbol("cloud.bolt", .nature),
+    Symbol("cloud.bolt.rain", .nature),
+    Symbol("cloud.sun", .nature),
+    Symbol("cloud.sun.rain", .nature),
+    Symbol("cloud.moon", .nature),
+    Symbol("cloud.moon.rain", .nature),
+    Symbol("cloud.moon.bolt", .nature),
+    Symbol("wind", .nature),
+    Symbol("wind.snow", .nature),
+    Symbol("snowflake", .nature),
+    Symbol("tornado", .nature),
+    Symbol("thermometer.sun", .nature),
+    Symbol("thermometer.snowflake", .nature),
+    Symbol("water.waves", .nature),
+    Symbol("drop", .nature),
+    Symbol("flame", .nature),
+    Symbol("bolt", .nature),
+    Symbol("mountain.2", .nature),
+    Symbol("hare", .nature),
+    Symbol("tortoise", .nature),
+    Symbol("lizard", .nature),
+    Symbol("bird", .nature),
+    Symbol("ant", .nature),
+    Symbol("ladybug", .nature),
+    Symbol("fish", .nature),
+    Symbol("pawprint", .nature),
+    Symbol("leaf", .nature),
+    Symbol("laurel.leading", .nature),
+    Symbol("camera.macro", .nature),
+    Symbol("tree", .nature),
+    Symbol("carrot", .nature),
+    Symbol("atom", .nature),
+    Symbol("fossil.shell", .nature),
+    
     //MARK: music
     Symbol("amplifier", .music),
     Symbol("dial.low", "dial", .music),
@@ -177,6 +131,18 @@ var symbols: [Symbol] = [
     Symbol("sailboat", .transportation),
     Symbol("scooter", .transportation),
     Symbol("tram", .transportation),
+    Symbol("airplane.arrival", .transportation),
+    Symbol("airplane.departure", .transportation),
+    Symbol("bus.doubledecker", .transportation),
+    Symbol("tram.fill.tunnel", .transportation),
+    Symbol("box.truck", .transportation),
+    Symbol("parkingsign", .transportation),
+    Symbol("engine.combustion", .transportation),
+    Symbol("oilcan", .transportation),
+    Symbol("steeringwheel", .transportation),
+    Symbol("car.side", .transportation),
+    Symbol("car.side.front.open.fill", .transportation),
+    Symbol("car.rear.road.lane", .transportation),
 
     //MARK: stationery
     Symbol("doc.text", "document", .stationery),
@@ -287,7 +253,34 @@ var symbols: [Symbol] = [
     Symbol("figure.roll", "roll", .people),
     Symbol("figure.stand", "stand", .people),
     Symbol("figure.walk", "walk", .people),
-    Symbol("figure.wave", "wave", .people)
-
+    Symbol("figure.wave", "wave", .people),
+    Symbol("figure.stand.line.dotted.figure.stand", .people),
+    Symbol("figure.2.arms.open", .people),
+    Symbol("figure.2.and.child.holdinghands", .people),
+    Symbol("figure.and.child.holdinghands", .people),
+    Symbol("hand.thumbsup", .people),
+    Symbol("hand.thumbsdown", .people),
+    Symbol("hand.point.up.left", .people)
 ]
+
+
+
+struct SymbolsList_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollView {
+            ForEach(symbolsCategory.allCases, id: \.self) { category in
+                Label(category.name, systemImage: category.symbol)
+                    .symbolVariant(.fill)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                SymbolGrid(color: .black, selectedSymbol: .constant(""), showSearch: .constant(false), category: category)
+                    
+            }
+        }
+    }
+}
+
+
+
 
